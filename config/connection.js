47f1a9445659477da320e2285c4b118f785aabc3
Tyler Ink Broeker <tylerinkbroeker@gmail.com>
2020-07-01 22:56:44 -0700

    added a readme

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Hellothere123!",
+    password: "",
     database: "burgers_db"
 });
 };
